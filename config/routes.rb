0cbe0123421f2523ae6c17273636e2448ffedc6d
Seth Geyer <seth.geyer@gmail.com>
2014-08-06 14:02:41 -0600

    resolved relative time

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,14 @@ Rails.application.routes.draw do
   post "signin" => "sessions#create"
   get "signout" => "sessions#destroy", as: :signout
   get "/about" => "abouts#about"
+
   get "/task_lists/new" => "task_lists#new"
   post "/task_lists" => "task_lists#create"
   get "/task_lists/:id/edit", to: "task_lists#edit"
   patch "/task_lists/:id", to: "task_lists#update"
+
+
+
 
   get "/tasks/new/:id", to: "tasks#new"
   post "/tasks", to: "tasks#create"
